bb662818524c8293a8bd5b34b388b769ef67b749
lukehasawii <may21luke@yahoo.com>
2019-07-14 17:17:14 -0500

    Added random sound out of 4 sounds

--- app/src/main/java/com/eoenesapps/myapplication/MainActivity.java
+++ app/src/main/java/com/eoenesapps/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.LinearLayoutCompat;
 
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -12,6 +13,7 @@ import android.media.*;
 
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,10 +27,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 //Placeholder
-
+    int list[] =
+        {R.raw.doorknock, R.raw.heavyrain, R.raw.morsecode, R.raw.spaceship};
+    Random r=new Random();
+    //String myArray[] = getResources().getStringArray(R.array.ids);
+    //tried to use the array in raw arrays.xml but it wouldnt convert
+    int randomNumber=r.nextInt(list.length);
    public void playSound(View v) {
         //Place music files in file called raw, then after that the name of the file
-       MediaPlayer mp = MediaPlayer.create(this, R.raw.heavyrain); //Placeholder file, change heavyrain to change what sound file it's playing
+       MediaPlayer mp = MediaPlayer.create(this, list[randomNumber]); //Placeholder file, change heavyrain to change what sound file it's playing
        mp.start();
 
 
