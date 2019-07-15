16a728fd61e219d802bf71330a01e1fa332e73e3
FireMiner99 <FireMiner99@users.noreply.github.com>
2019-07-14 19:40:45 -0500

    The App is working now

--- app/src/main/java/com/eoenesapps/myapplication/MainActivity.java
+++ app/src/main/java/com/eoenesapps/myapplication/MainActivity.java
@@ -26,14 +26,33 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
     }
-//Placeholder
+
     int list[] =
         {R.raw.doorknock, R.raw.heavyrain, R.raw.morsecode, R.raw.spaceship};
-    Random r=new Random();
+    Random r = new Random();
     //String myArray[] = getResources().getStringArray(R.array.ids);
     //tried to use the array in raw arrays.xml but it wouldnt convert
-    int randomNumber=r.nextInt(list.length);
-   public void playSound(View v) {
+    int randomNumber = r.nextInt(list.length);
+
+
+    protected void onStart()
+    {
+        super.onStart();
+
+        int list[] =
+                {R.raw.doorknock, R.raw.heavyrain, R.raw.morsecode, R.raw.spaceship};
+        Random r = new Random();
+        //String myArray[] = getResources().getStringArray(R.array.ids);
+        //tried to use the array in raw arrays.xml but it wouldnt convert
+        int randomNumber = r.nextInt(list.length);
+        MediaPlayer mp = MediaPlayer.create(this, list[randomNumber]); //Placeholder file, change heavyrain to change what sound file it's playing
+        mp.start();
+    }
+
+
+
+
+    public void playSound(View v) {
         //Place music files in file called raw, then after that the name of the file
        MediaPlayer mp = MediaPlayer.create(this, list[randomNumber]); //Placeholder file, change heavyrain to change what sound file it's playing
        mp.start();
