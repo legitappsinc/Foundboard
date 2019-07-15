61a75297b17d1eb83937253cc882a14a4e2e2fb5
FireMiner99 <FireMiner99@users.noreply.github.com>
2019-07-15 14:55:53 -0500

    Fixed a bug

--- app/src/main/java/com/eoenesapps/myapplication/info.java
+++ app/src/main/java/com/eoenesapps/myapplication/info.java
@@ -3,6 +3,7 @@ package com.eoenesapps.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class info extends AppCompatActivity {
 
@@ -11,4 +12,9 @@ public class info extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
     }
+
+    public void onBackPress(View v){
+        setContentView(R.layout.activity_start_screen);
+    }
+
 }
